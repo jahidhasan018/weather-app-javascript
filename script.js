c96f87444c2a1b87b52b5ab1c19a7a1e9c01b916
Jahid Hasan <jahid0676@gmail.com>
2023-12-27 22:04:25 +0600

    Final Commit

--- script.js
+++ script.js
@@ -1,70 +1,91 @@
 // Key = b3b51e981352049d5c4838914eb2d1ee
 // Api = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=b3b51e981352049d5c4838914eb2d1ee
 
-const Key = "b3b51e981352049d5c4838914eb2d1ee";
-const searchInput = document.querySelector("#search-city");
-const searchBtn = document.querySelector("#search-btn");
+const Key           = "b3b51e981352049d5c4838914eb2d1ee";
+const searchInput   = document.querySelector("#search-input");
+const searchBtn     = document.querySelector("#search-btn");
+const error         = document.querySelector("#error");
+const weatherBox    = document.querySelector(".weather-details");
+const weatherIcon   = document.querySelector(".weather-icon");
+const city          = document.querySelector(".city");
+const condition     = document.querySelector(".weather-condition");
+const temppresure   = document.querySelector(".temp");
+const humidityDiv   = document.querySelector(".humidity");
+const wind          = document.querySelector(".wind");
+
+// show function
+const getError = (msg) => {    
+    error.innerHTML = msg;
+    error.style = "display: block;";
+    weatherBox.style = "display: none;";
+}
 
 
-searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const city = searchInput.value;
-    if (city) {
-        getWeatherByLocation(city);
+// Async function to get weather data
+async function getWeatherData(value) {
+    const cityName = value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${Key}&units=metric`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Error handling if city not found
+    if(response.status == 404 ) {
+        getError("Invaild city name");
+        return;
     }
+
+    if( value == "" ) {
+        getError("Please enter city name");
+    }
+
+    // If success then show weather data
+    if(data.cod == 200) {
+        showWeatherData(data);
+        weatherBox.style = "display: block;";
+        error.style = "display: none;";
+    }
+} 
+
+// Show weather data
+const showWeatherData = (weatherData) => {
+    const { name } = weatherData;
+    const { temp, humidity } = weatherData.main;
+    const { speed } = weatherData.wind;
+    const weatherCondition  = weatherData.weather[0].main;
+
+    condition.innerHTML = weatherCondition;
+    city.innerHTML = name;
+    temppresure.innerHTML = Math.round(temp) + "°C";
+    humidityDiv.innerHTML = humidity + "%";
+    wind.innerHTML = speed + " km/h";
+
+    if(weatherCondition == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    }else if(weatherCondition == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    }else if(weatherCondition == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    }else if(weatherCondition == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    }else if(weatherCondition == "Snow") {
+        weatherIcon.src = "images/snow.png";
+    }else if(weatherCondition == "Mist") {
+        weatherIcon.src = "images/mist.png";
+    }else{
+        weatherIcon.src = "images/clear.png";
+    }
+}
+
+
+// Click Event listener
+searchBtn.addEventListener("click", () => {
+    getWeatherData(searchInput.value);
 });
 
-function getWeatherByLocation(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Key}&units=metric`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            showWeather(data);
-        });
-}
-
-function showWeather(data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    const { country } = data.sys;
-
-    document.querySelector(".city").innerText = `Weather in ${name}, ${country}`;
-    document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = `${temp}°C`;
-    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
-    document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`;
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
-}
-
-function currentDate() {
-    const now = new Date();
-    const date = document.querySelector(".date");
-    date.innerText = dateBuilder(now);
-}
-
-function dateBuilder(d) {
-    const months = [
-        "January", "February", "March", "April", "May", "June", "July",
-        "August", "September", "October", "November",
-        "December"
-    ];
-    const days = [
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-    ];
-    const day = days[d.getDay()];
-    const date = d.getDate();
-    const month = months[d.getMonth()];
-    const year = d.getFullYear();
-    return `${day}, ${date} ${month} ${year}`;
-}
-
-currentDate();
-
-// https://source.unsplash.com/1600x900/?london
-// https://source.unsplash.com/1600x900/?paris
-// https://source.unsplash.com/1600x900/?tokyo
+// Enter key event listener
+searchInput.addEventListener("keyup", (event) => {
+    if(event.key == "Enter") {
+        getWeatherData(searchInput.value);
+    }
+});
